c068ed71a6b64cd04b58782819ab08859eb1dcc8
JNaeder <39416919+JNaeder@users.noreply.github.com>
2022-01-02 12:36:16 -0500

    Added Tonal.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // Document Stuff
 const generateButton = document.getElementById("generate_button");
 const scaleOutput = document.getElementById("output");
+const notesCheckbox = document.getElementById("note-names");
+const rhythmCheckbox = document.getElementById("rhythm");
+const playingStyleCheckbox = document.getElementById("playing-style");
 
 // Scale Variables
 const keys = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
 const signs = ['♮', '♭', '♯'];
-const modes = ['Major', 'Dorian', 'Phrygian', 'Lydian', 'Mixolydian', 'Minor', 'Locrian', 'Minor Pentatonic', 'Major Pentatonic']
+const modes = ['Major', 'Dorian', 'Phrygian', 'Lydian', 'Mixolydian', 'Minor', 'Locrian', 'Minor Pentatonic', 'Major Pentatonic', 'Major Triad Arpeggio', 'Minor Triad Arpeggio', 'Dom7 Arpeggio']
 const rhythm = ['16ths', '8ths', 'Triplets', 'Sectuplets'];
 const style = ['Straight'];
 
@@ -15,11 +18,11 @@ generateButton.addEventListener('click', makeNewScale);
 // Functions
 
 function makeNewScale(){
-    const randomKey = createRandom(keys);
-    const randomSign = createRandom(signs);
+    const randomKey = notesCheckbox.checked ? createRandom(keys) : "";
+    const randomSign = notesCheckbox.checked ? createRandom(signs) : "";
     const randomMode = createRandom(modes);
-    const randomRhythm = createRandom(rhythm);
-    scaleOutput.innerHTML = `${randomKey}${randomSign} ${randomMode} ${randomRhythm}`;
+    const randomRhythm = rhythmCheckbox.checked ? ` - ${createRandom(rhythm)}` : "";
+    scaleOutput.innerHTML = `${randomKey}${randomSign} ${randomMode}${randomRhythm}`;
 }
 
 function createRandom(item){
